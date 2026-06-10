724b6ce0f8169e378f1bc065f5991a11fae21615
TK Cho <tkcho@MacBookAir.lan>
2026-06-10 14:55:27 -0400

    fix: hardcode supabase key

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://gvyycqxsezrzktfjzmsj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2eXljcXhzZXpyemt0ZmpqbXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyMTY2MDQsImV4cCI6MjA2Mzc5MjYwNH0.saRIwpGk8e23YGJC41kKvmbYldYkUbcISMrR-jqYl7w'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
